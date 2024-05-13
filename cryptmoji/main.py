012d66684786b329782105f0f32b2440bd0dfc2b
Siddhesh Agarwal <siddhesh.agarwal@gmail.com>
2024-05-13 13:09:02 +0530

    fixed type hinting

--- cryptmoji/main.py
+++ cryptmoji/main.py
@@ -1,9 +1,10 @@
-from data import EMOJIS
+from typing import Union
+from cryptmoji.data import EMOJIS
 
 LENGTH = len(EMOJIS)
 
 
-def encrypt(text: str, *, key: str = None) -> str:
+def encrypt(text: str, *, key: Union[str, None] = None) -> str:
     """
     Encrypts a string to emojis.
 
@@ -19,15 +20,16 @@ def encrypt(text: str, *, key: str = None) -> str:
     encrypted: str
         The encrypted string
     """
+    iterator = range(len(text))
     if key is None:
-        char_2_int = [ord(text[i]) for i in range(len(text))]
+        char_2_int = [ord(text[i]) for i in iterator]
     else:  # key is not None
-        char_2_int = [ord(text[i]) + ord(key[i % len(key)]) for i in range(len(text))]
-    text = [EMOJIS[i % LENGTH] for i in char_2_int]
-    return "".join(text)
+        char_2_int = [ord(text[i]) + ord(key[i % len(key)]) for i in iterator]
+    char_arr = [EMOJIS[i % LENGTH] for i in char_2_int]
+    return "".join(char_arr)
 
 
-def decrypt(text: str, *, key: str = None) -> str:
+def decrypt(text: str, *, key: Union[str, None] = None) -> str:
     """
     Decrypts a string from emojis.
 
@@ -44,19 +46,10 @@ def decrypt(text: str, *, key: str = None) -> str:
         The decrypted string
     """
     emojis_2_int = [EMOJIS.index(i) for i in text]
+    iterator = range(len(emojis_2_int))
     if key is not None:
-        decrypted = [
-            emojis_2_int[i] - ord(key[i % len(key)]) for i in range(len(emojis_2_int))
-        ]
+        decrypted = [emojis_2_int[i] - ord(key[i % len(key)]) for i in iterator]
     else:
-        decrypted = [emojis_2_int[i] for i in range(len(emojis_2_int))]
-    text = [chr(i) for i in decrypted]
-    return "".join(text)
-
-if __name__ == "__main__":
-    text = "Hello World!"
-    encrypted = encrypt(text)
-    decrypted = decrypt(encrypted)
-    print(f"Text: {text}")
-    print(f"Encrypted: {encrypted}")
-    print(f"Decrypted: {decrypted}")
+        decrypted = [emojis_2_int[i] for i in iterator]
+    char_arr = [chr(i) for i in decrypted]
+    return "".join(char_arr)
