af68cbe35b892daee0182597d04cf210f22c1f18
Siddhesh Agarwal <siddhesh.agarwal@gmail.com>
2024-05-14 09:10:51 +0530

    updated CLI

--- cryptmoji/cli.py
+++ cryptmoji/cli.py
@@ -1,6 +1,5 @@
-from typing import Annotated, Union
 from rich.console import Console
-from typer import Typer, Option
+from typer import Typer, prompt
 
 from cryptmoji.main import decrypt, encrypt
 from cryptmoji.version import __version__
@@ -8,27 +7,18 @@ from cryptmoji.version import __version__
 app = Typer()
 console = Console()
 
-key_option_type = Option(
-    "--key",
-    help="This is an optional key that can be used to increase the complexity of the encryption.",
-)
-
 
 @app.command("encrypt")
-def cli_encrypt(
-    text: str,
-    key: Annotated[Union[str, None], key_option_type] = None,
-):
+def cli_encrypt(text: str):
     """Encrypts a string as a string of emojis."""
+    key = prompt("Enter key", default=None)
     console.print(encrypt(text, key=key))
 
 
 @app.command("decrypt")
-def cli_decrypt(
-    text: str,
-    key: Annotated[Union[str, None], key_option_type] = None,
-):
+def cli_decrypt(text: str):
     """Decrypts a string of emojis."""
+    key = prompt("Enter key", default=None)
     console.print(decrypt(text, key=key))
 
 
