c6dfec0741a8f50511e6f00c2e7c3cbce2c849d1
Siddhesh Agarwal <siddhesh.agarwal@gmail.com>
2024-05-15 21:48:20 +0530

    fixed CLI

--- cryptmoji/cli.py
+++ cryptmoji/cli.py
@@ -1,6 +1,5 @@
-from typing import Annotated
 from rich.console import Console
-from typer import Typer, prompt, Option
+from typer import Typer, prompt
 
 from cryptmoji.main import decrypt, encrypt
 from cryptmoji.version import __version__
@@ -12,24 +11,34 @@ console = Console()
 @app.command("encrypt")
 def cli_encrypt(text: str):
     """Encrypts a string as a string of emojis."""
-    key = prompt("Enter key", default=None)
-    console.print(encrypt(text, key=key))
+    key = prompt("Enter key (optional)", default="", show_default=False)
+    key = key if key else None
+    print(f"{key=}")
+    console.print(f"The encrypted string is: {encrypt(text, key=key)}")
 
 
 @app.command("decrypt")
 def cli_decrypt(text: str):
     """Decrypts a string of emojis."""
-    key = prompt("Enter key", default=None)
-    console.print(decrypt(text, key=key))
+    key = prompt("Enter key (optional)", default="", show_default=False)
+    key = key if key else None
+    print(f"{key=}")
+    console.print(f"The decrypted string is: {decrypt(text, key=key)}")
 
 
-def cli_version(
-    show_version: Annotated[bool, Option("--version", "-v", is_eager=True)]
-):
+@app.command("version")
+def cli_version():
     """Prints the version of the library."""
     version = ".".join(str(i) for i in __version__)
-    if show_version:
-        console.print(f"cryptmoji v{version}")
+    console.print(f"cryptmoji [bold green]v{version}[/]")
+
+
+@app.command("docs")
+def cli_docs():
+    """Launches the documentation in the browser."""
+    import webbrowser
+
+    webbrowser.open("https://github.com/Siddhesh-Agarwal/cryptmoji/wiki")
 
 
 if __name__ == "__main__":
